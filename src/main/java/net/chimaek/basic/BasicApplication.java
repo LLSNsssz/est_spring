6f53c474f7c273bc13979394e429f25a24163265
LLSNsssz <seungjang93@gmail.com>
2024-07-09 01:25:00 +0900

    init

--- src/main/java/net/chimaek/basic/BasicApplication.java
+++ src/main/java/net/chimaek/basic/BasicApplication.java
@@ -1,19 +1,25 @@
 package net.chimaek.basic;
 
-import net.chimaek.basic.service.MyService;
-import org.springframework.boot.SpringApplication;
+import jakarta.servlet.ServletException;
+import java.io.IOException;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.DispatcherServlet;
 
 @SpringBootApplication(
     scanBasePackages = "net.chimaek"
 )
 public class BasicApplication {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws ServletException, IOException {
+    DispatcherServlet dispatcherServlet = new DispatcherServlet();
+    String url = "/hello";
 
-    MyService myService = new MyService();
-    myService.hello();
-    SpringApplication.run(BasicApplication.class, args);
+    MockHttpServletRequest request = new MockHttpServletRequest(url);
+    MockHttpServletResponse response = new MockHttpServletResponse();
+
+    dispatcherServlet.service(request, response);
+
+    System.out.println(response.getContentType());
+    System.out.println("응답 내용" + response.getContentType());
   }
-
 }
