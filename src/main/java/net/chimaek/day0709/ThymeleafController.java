b840fdeb436a43160cf04412c1e0be9e216e83a4
LLSNsssz <seungjang93@gmail.com>
2024-07-11 17:39:23 +0900

    day0709

--- src/main/java/net/chimaek/day0709/ThymeleafController.java
+++ src/main/java/net/chimaek/day0709/ThymeleafController.java
@@ -3,15 +3,14 @@ package net.chimaek.day0709;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ThymeleafController {
 
-    @GetMapping("/index123")
-    public String index(Model model) {
-        model.addAttribute("name", "쏘굿");
-        model.addAttribute("name2", "굿굿");
-        model.addAttribute("label1", "로긴");
+    @GetMapping("/index")
+    public String index(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name1", name);
         return "index";
     }
 }
