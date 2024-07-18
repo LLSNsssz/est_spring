4af288f697c6dac1d65516c7c4548754ae8854f6
LLSNsssz <seungjang93@gmail.com>
2024-07-18 15:36:27 +0900

    day0718 add

--- src/main/java/net/chimaek/day0717_restapi/BoardPostService.java
+++ src/main/java/net/chimaek/day0717_restapi/BoardPostService.java
@@ -1,11 +1,14 @@
 package net.chimaek.day0717_restapi;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class BoardPostService {
 
     List<BoardPost> boardPosts = new ArrayList<>();
@@ -13,10 +16,14 @@ public class BoardPostService {
     private Long nextCommentId = 1L;
 
     public BoardPostDTO createBoardPost(BoardPostDTO boardPostDTO) {
-        return convertToBoardPostEntity(boardPostDTO);
+        BoardPost boardPost = convertToBoardPostEntity(boardPostDTO);
+        boardPost.setId(nextPostId++);
+        boardPost.setCreateAt(LocalDateTime.now());
+        boardPosts.add(boardPost);
+        return convertToBoardPostDto(boardPost);
     }
 
-    private static BoardPost convertToBoardPostEntity(BoardPostDTO boardPostDTO) {
+    private BoardPost convertToBoardPostEntity(BoardPostDTO boardPostDTO) {
         BoardPost boardPost = new BoardPost();
         boardPost.setTitle(boardPostDTO.getTitle());
         boardPost.setContent(boardPostDTO.getContent());
@@ -31,7 +38,7 @@ public class BoardPostService {
         return boardPost;
     }
 
-    private static BoardPostDTO convertToBoardPostDto(BoardPost boardPost) {
+    private BoardPostDTO convertToBoardPostDto(BoardPost boardPost) {
         BoardPostDTO boardPostDTO = new BoardPostDTO();
         boardPostDTO.setId(boardPost.getId());
         boardPostDTO.setTitle(boardPost.getTitle());
@@ -47,5 +54,77 @@ public class BoardPostService {
             );
         }
         return boardPostDTO;
+    }
+
+    private Comment convertToCommentEntity(CommentDTO commentDTO) {
+        Comment comment = new Comment();
+        comment.setId(commentDTO.getId());
+        comment.setContent(commentDTO.getContent());
+        comment.setAuthor(commentDTO.getAuthor());
+        return comment;
+    }
+
+    private CommentDTO convertToCommentDto(Comment comment) {
+        CommentDTO commentDTO = new CommentDTO();
+        commentDTO.setId(comment.getId());
+        commentDTO.setContent(comment.getContent());
+        commentDTO.setAuthor(comment.getAuthor());
+        commentDTO.setCreateAt(comment.getCreateAt());
+        return commentDTO;
+    }
+
+    public List<BoardPostDTO> getAllBoardPostsDto() {
+        return boardPosts.stream().map(this::convertToBoardPostDto).collect(Collectors.toList());
+    }
+
+    public BoardPostDTO getBoardPostDtoById(Long id) {
+        log.info("헿헿헿");
+        return boardPosts.stream()
+            .filter(b -> b.getId().equals(id))
+            .map(this::convertToBoardPostDto)
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("그런 글 없습니다."));
+    }
+
+    public void deleteBoardPost(Long id) {
+        boardPosts.remove(findBoardPostById(id));
+    }
+
+    private BoardPost findBoardPostById(Long id) {
+        return boardPosts.stream()
+            .filter(post -> post.getId().equals(id))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("id에 해당하는 글을 찾을수 없습니다."));
+    }
+
+    public BoardPostDTO updateBoardPost(Long id, BoardPostDTO updateBoardPostDto) {
+        BoardPost boardPostById = findBoardPostById(id);
+        boardPostById.setTitle(updateBoardPostDto.getTitle());
+        boardPostById.setContent(updateBoardPostDto.getContent());
+        boardPostById.setAuthor(updateBoardPostDto.getAuthor());
+        boardPostById.setUpdateAt(LocalDateTime.now());
+        return convertToBoardPostDto(boardPostById);
+    }
+
+    public CommentDTO createComment(Long postId, CommentDTO createCommentDTO) {
+        BoardPost boardPostById = findBoardPostById(postId);
+        Comment comment = convertToCommentEntity(createCommentDTO);
+        comment.setId(nextCommentId++);
+        comment.setCreateAt(LocalDateTime.now());
+        boardPostById.addComment(comment);
+        return convertToCommentDto(comment);
+    }
+
+    public void deleteComment(Long postId, Long commentId) {
+        BoardPost boardPost = findBoardPostById(postId);
+        Comment commentById = findCommentById(commentId, boardPost);
+        boardPost.removeComment(commentById);
+    }
+
+    private Comment findCommentById(Long commentId, BoardPost boardPost) {
+        return boardPost.getComments().stream()
+            .filter(c -> c.getId().equals(commentId))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("댓글을 찾을 수 없습니다."));
     }
 }
